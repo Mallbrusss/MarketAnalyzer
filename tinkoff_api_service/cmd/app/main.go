f832e59d021d31e1abaa77a585db052fe00b8fec
e.bogachev <e.bogachev@digitaleconomy.space>
2024-11-22 17:06:45 +0300

    test method

--- tinkoff_api_service/cmd/app/main.go
+++ tinkoff_api_service/cmd/app/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-
+	"log"
+	"net/http"
+	"tinkoff-api/config"
+	"tinkoff-api/internal/handlers"
+	"tinkoff-api/internal/services"
 )
 
 func main() {
+	cfg := config.LoadConfig()
 
+	service := services.NewTinkoffService(cfg)
+	handler := handlers.NewHandler(service)
+
+	http.HandleFunc("/api/v1/closePrices", handler.GetClosePricesHandler)
+
+	log.Printf("Server is running on port %s...", cfg.ServerPort)
+	log.Fatal(http.ListenAndServe(":"+cfg.ServerPort, nil))
 }
